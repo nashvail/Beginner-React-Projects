d870ed318ae9bf58c9c1309b0894974b66bb96b8
nashmaniac <nishantve1@gmail.com>
2015-07-10 12:49:11 +0530

    Added second beginner project

---
+++ React-Font-Tester/main.jsx
@@ -0,0 +1,64 @@
+var App = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<Input />
+				<Output font = "Karla" />
+				<Output font = "Helvetica Neue" />
+				<Output font = "Bree serif" />
+				<Output font = "Lato" />
+				<Output font = "Cabin" />
+				<Output font = "Proxima Nova" />
+				<Output font = "Andale Mono" />
+			</div>
+		);
+	}
+});
+
+// The main input field
+var Input = React.createClass({
+	// Now we need a funciton that fires up everytime we start typing in the input and then updates the relevant text 
+	update: function() {
+		var currentValue = this.refs.main_input.getDOMNode().value;
+		var outputNodes = document.getElementsByClassName('output');
+		for(outputNode in outputNodes) {
+			if(outputNodes.hasOwnProperty(outputNode)) {
+				outputNodes[outputNode].textContent = currentValue;
+			}
+		}
+	},
+	render: function() {
+		return(
+			<div>
+				<input ref = "main_input" type = "text" id = "main_input" name = "main_input" onChange = {this.update} placeholder = "Enter some text" />
+			</div>
+		);
+	}
+});
+
+// This is the component in which the output text will be displayed
+var Output = React.createClass({
+	getDefaultProps: function () {
+	    return {
+	        val: "Hello, world!"  
+	    };
+	},
+	render: function() {
+		var divStyle = {
+			fontFamily : this.props.font,
+			fontSize : 28
+		};
+		return(
+			<div className = "fontOutput">
+				<div className = "fontName">
+					{this.props.font}
+				</div>
+				<div style = {divStyle} className = "output">
+					{this.props.val}
+				</div>
+			</div>
+		);
+	}
+});
+
+React.render(<App/>, document.getElementById('app'));
